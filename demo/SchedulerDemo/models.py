ba6fe57ffe15ed8b74024fd34184e89653c17542
t_barun <pujanthapa88.63@gmail.com>
2018-02-27 15:14:23 +0545

    Models for tash and scheduled task

--- demo/SchedulerDemo/models.py
+++ demo/SchedulerDemo/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import requests
+from rq import get_current_job
 
-# Create your models here.
+
+class Task(models.Model):
+    # A model to save information about an asynchronous task
+    created_on = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=128)
+    job_id = models.CharField(max_length=128)
+    result = models.CharField(max_length=128, blank=True, null=True)
+
+
+class ScheduledTask(models.Model):
+    # A model to save information about a scheduled task
+    created_on = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=128)
+    # A scheduled task has a common job id for all its occurences
+    job_id = models.CharField(max_length=128)
+
+
+class ScheduledTaskInstance(models.Model):
+    # A model to save information about instances of a scheduled task
+    scheduled_task = models.ForeignKey('ScheduledTask', on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    result = models.CharField(max_length=128, blank=True, null=True)
