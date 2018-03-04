1b8b35235ff5b4bec7838fd023d3a3ce0a438ac9
t_barun <pujanthapa88.63@gmail.com>
2018-03-04 14:35:16 +0545

    Added a new model Coupon

--- demo/SchedulerDemo/admin.py
+++ demo/SchedulerDemo/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from SchedulerDemo.models import Task, ScheduledTask, ScheduledTaskInstance
+from SchedulerDemo.models import Task, ScheduledTask, ScheduledTaskInstance, Coupon
 
 
 class TaskAdmin(admin.ModelAdmin):
@@ -15,6 +15,12 @@ class ScheduledTaskAdmin(admin.ModelAdmin):
 class ScheduledTaskInstanceAdmin(admin.ModelAdmin):
     pass
 
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['created_on', 'name', 'type']
+
+
 admin.site.register(Task, TaskAdmin)
 admin.site.register(ScheduledTask, ScheduledTaskAdmin)
 admin.site.register(ScheduledTaskInstance, ScheduledTaskInstanceAdmin)
+admin.site.register(Coupon, CouponAdmin)
--- demo/SchedulerDemo/models.py
+++ demo/SchedulerDemo/models.py
@@ -1,7 +1,19 @@
+import datetime
 from django.db import models
-import requests
-from rq import get_current_job
+from django.utils.translation import ugettext_lazy as _
 
+GET = "get"
+SAVE = "save"
+COUPON_HASH_TYPES = (
+    (GET, _("GET")),
+    (SAVE, _("SAVE"))
+)
+
+
+class Coupon(models.Model):
+    name = models.CharField(max_length=100, default="A coupon", blank = False)
+    created_on = models.DateTimeField(_("Created On"),blank=False, default = datetime.datetime.now())
+    type = models.CharField(_("Type"), max_length=5, choices=COUPON_HASH_TYPES)
 
 class Task(models.Model):
     # A model to save information about an asynchronous task
