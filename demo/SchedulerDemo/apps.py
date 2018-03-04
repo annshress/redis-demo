bf1a9f7b35f5ad331cc3a7031f8ea6ac5eafa8e2
t_barun <pujanthapa88.63@gmail.com>
2018-03-04 14:39:31 +0545

    Added a scheduling function to run infinitly when the server starts

--- demo/SchedulerDemo/apps.py
+++ demo/SchedulerDemo/apps.py
@@ -1,5 +1,18 @@
+import datetime
+
 from django.apps import AppConfig
 
 
 class SchedulerdemoConfig(AppConfig):
     name = 'SchedulerDemo'
+
+    def ready(self):
+        import django_rq
+        from SchedulerDemo.tasks import change_coupon_type
+        timenow = datetime.datetime.now()
+        scheduler = django_rq.get_scheduler('default')
+        job = scheduler.schedule(
+            scheduled_time=timenow,
+            func=change_coupon_type,
+            interval=5,
+        )
